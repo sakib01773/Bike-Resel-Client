63ecdf94ca23538f3cbd7d03b7bca40ca538d2ea
sakib01773 <sakibshohan1@gmail.com>
2022-11-29 02:19:24 +0600

    hocks done without token

---
+++ src/hocks/useToken.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+const useAdmmin = email =>{
+    const [isAdmin, setIsAdmin] = useState(false)
+
+    const [isAdminLoading, setIsAdminLoadig] = useState(true)
+
+    useEffect( () =>{
+        if(email){
+            fetch(`http://localhost:5000/users/admin/${email}`)
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data)
+                setIsAdmin(data.isAdmin)
+                
+                setIsAdminLoadig(false)
+            })
+        }
+    },[email])
+    return [isAdmin, isAdminLoading]
+}
+
+export default useAdmmin;
